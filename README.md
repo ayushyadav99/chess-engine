0549c1299d690ab83c12d8d728001a853fe7653d
Mindjolt2406 <rathin7@gmail.com>
2021-02-23 13:11:45 +0530

    Updated README and gave Colour to GameUser

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 # chess-engine
-
-## UML Diagram
-https://drive.google.com/file/d/1ckRfJpZRluJmtxhC1eGvQrwz1NAxzGf0/view?usp=sharing
--- src/main/java/com/chess/backend/game/GameUser.java
+++ src/main/java/com/chess/backend/game/GameUser.java
@@ -1,5 +1,6 @@
 package com.chess.backend.game;
 
+import com.chess.backend.core.Colour;
 import com.chess.backend.core.Move;
 import com.chess.backend.core.State;
 import lombok.AllArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public abstract class GameUser {
     ChessClock chessClock;
+    Colour colour;
 
     public abstract Move makeMove(State state);
 }
